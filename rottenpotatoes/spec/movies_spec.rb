ac3ed39515c651d81a78c148cf8df7200a63323a
Zhale Nowroozilarki <76499517+zhalenl@users.noreply.github.com>
2021-10-14 21:31:30 -0500

    Done

---
+++ rottenpotatoes/spec/movies_spec.rb
@@ -0,0 +1,13 @@
+# require 'rails_helper'
+
+# RSpec.describe "Movies", type: :request do
+#   describe "GET /movies" do
+#     m=FactoryBot.create(:movie) 
+#      it "contains the product names" do
+#         get "/movies"
+#       #  puts movies[0].title
+#         expect(responce.body).to include m.title
+       
+#      end
+#   end
+# end
